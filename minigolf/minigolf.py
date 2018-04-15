069d6f36b0a173c5d9ebd4e06685d925ce219102
Pavel <pfedorov7721@gmail.com>
2018-04-15 05:21:43 +0300

    All tests passed

--- minigolf/minigolf.py
+++ minigolf/minigolf.py
@@ -4,6 +4,7 @@ class Player:
     def __init__(self, player):
         self._player = player
         self.score = 1
+        self.attempt = 0
 
     @property
     def name(self):
@@ -15,7 +16,6 @@ class HitsMatch:
 
         self.numbers = numbers
         self.players = players
-        self.status = True
 
         self.value = 0
         self.hole = 1
@@ -40,7 +40,6 @@ class HitsMatch:
                     self.value = 0
 
             if(len(self.big) == 0):
-                self.status = False
                 self.value = self.hole
                 self.big = [i for i in range(0, self.numbers)]
                 self.hole += 1
@@ -88,4 +87,101 @@ class HitsMatch:
             raise RuntimeError('The match is over')
 
 class HolesMatch:
-    pass
+    
+    nice_hit = 0
+    hole = 1
+
+    def __init__(self, numbers, players):
+
+        self.numbers = numbers
+        self.players = players
+
+        self.last_player = players[-1].name
+        self.generator = HolesMatch.generate_players(numbers, players)
+        self.table = HolesMatch.default_table(numbers, players)
+
+    @staticmethod
+    def generate_players(numbers, players):
+        iterations = numbers * 10
+        if(HolesMatch.hole > 1):
+            players.append(players.pop(0))
+        while (iterations):
+            for value in range(0, numbers):
+                yield players[value]
+            iterations -+ 1
+
+    @staticmethod
+    def default_table(numbers, players):
+        none_line = tuple([ None for i in range(0,len(players))])
+        name_line = tuple(player.name for player in players)
+        table = [none_line for i in range(1, numbers + 2)] 
+        table[0] = name_line
+        return table
+
+    def hit(self, success = False):
+
+        next_player = next(self.generator)
+        if (HolesMatch.hole <= self.numbers):
+            if(success):
+                next_player.score = 1
+                self.make_table(next_player)
+                HolesMatch.nice_hit = 1
+                next_player.attempt = 0
+
+            else:
+                next_player.score = 0
+                next_player.attempt += 1
+                if (HolesMatch.nice_hit):
+                    self.make_table(next_player)
+                    if (next_player.name == self.players[-1].name):
+                        self.next_hole()
+
+                if (next_player.attempt == 10):
+                    self.make_table(next_player)
+                    if (next_player.name == self.players[-1].name):
+                        self.next_hole()
+        else:
+            raise RuntimeError('The match is over')
+
+    def next_hole(self):
+        self.generator = HolesMatch.generate_players(self.numbers, self.players)
+        HolesMatch.hole += 1
+        HolesMatch.nice_hit = 0
+
+    def make_table(self, next_player):
+
+        my_list = list(self.table[0])
+        player_index = my_list.index(next_player.name)
+
+        my_list1 = list(self.table[HolesMatch.hole])
+        my_list1[player_index] = next_player.score
+
+        my_tuple = tuple(my_list1)
+        self.table[HolesMatch.hole] = my_tuple
+
+    @property
+    def finished(self):
+        return HolesMatch.hole > self.numbers
+
+    def get_table(self):
+        return self.table
+
+    def get_winners(self):
+        if (HolesMatch.hole > self.numbers):  
+
+            total = collections.defaultdict(int)
+            for line in self.table[1:]:
+                for index, player in enumerate(line):
+                    total[index] += player
+
+            last_list = sorted(total.items(), key=lambda x: x[1])
+            max_score = last_list[-1][1]
+            champions = []
+            for champion in total:
+                if(max_score == total[champion]):
+                    champions.append(self.players[champion])
+                else:
+                    break
+            return champions
+        else:
+            raise RuntimeError('The match is over')
