bac4c1e604de0d02a0dc482bde88924fb4b1b9d1
Pavel <pfedorov7721@gmail.com>
2018-05-24 13:37:26 +0300

    last version. OOP and syntax left

--- async_file_storage/aiohttp_polls/main.py
+++ async_file_storage/aiohttp_polls/main.py
@@ -6,27 +6,29 @@ import urllib.request
 from settings import get_config
 import asyncio
 import argparse
-import weakref
 import pprint
 import sys
 
 async def handle(request):
 	filename = request.match_info.get('file')
 	if(filename.endswith('.')):
-		try:
-			new_filename  =  filename[0:-1]
-			path_to_file = directory + new_filename
-			file = open(path_to_file)
-			my_buffer = file.read()
-			return web.Response(text=my_buffer)
-		except FileNotFoundError:
-			return web.Response(text='404')
+
+		new_filename  =  filename[0:-1]
+		path_to_file = directory + new_filename
+
+		t2 = Reader(path_to_file)
+		t2.start()
+		t2.join()
+
+		return web.Response(text=t2.get_file())
 	else:
-		try:
-			path_to_file = directory + filename
-			file = open(path_to_file)
-			return web.Response(text=file.read())
-		except FileNotFoundError:
+		path_to_file = directory + filename
+
+		t = Reader(path_to_file)
+		t.start()
+		t.join()
+
+		if(t.get_file() == '404'):
 			filename = filename + '.'
 			counter = 0
 			while(True):
@@ -34,11 +36,50 @@ async def handle(request):
 					t1 = loop.create_task(ask_nodes(filename, item))
 					buff = await t1
 					if(buff != '404'):
+						t4 = Writer(buff, path_to_file)
+						t4.start()
+						t4.join()
 						return buff
 					else:
 						counter += 1
 						if (counter == len(nodes.values())):
 							return web.Response(text='404')
+		return web.Response(text=t.get_file())
+
+
+class Reader(threading.Thread):
+	
+	def __init__(self, filename):
+		threading.Thread.__init__(self)
+		self.daemon = True
+		self.filename = filename
+		self.buf = ''
+
+	def run(self):
+		try:
+			f = open(self.filename)
+			self.buf = f.read()
+			f.close()
+		except FileNotFoundError:
+			self.buf = '404'
+
+	def get_file(self):
+		return self.buf
+
+class Writer(threading.Thread):
+
+	def __init__(self, buf, path):
+		threading.Thread.__init__(self)
+		self.daemon = True
+		self.path = path
+		self.buf = buf
+
+	def run(self):
+
+		f = open(self.path, "w")
+		f.write(self.buf)
+		f.close()
+
 
 
 async def ask_nodes(filename, item):
@@ -47,7 +88,6 @@ async def ask_nodes(filename, item):
 		if(response == '404'):
 			return '404'
 		return web.Response(text=response)
-
 
 async def fetch(session, url):
 	async with session.get(url) as response:
