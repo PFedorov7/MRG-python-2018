92a91bce1d8a054fff15820d70b2a85ed033cc08
Pavel <pfedorov7721@gmail.com>
2018-05-24 14:20:45 +0300

    delete test modules

--- async_file_storage/aiohttp_polls/routes.py
+++
@@ -1,8 +0,0 @@
-from views import handle
-from aiohttp import web
-
-def setup_routes(app):
-	app.add_routes([
-	    web.get('/', handle),
-	    web.get('/{file}', handle),
-	])
--- async_file_storage/aiohttp_polls/views.py
+++
@@ -1,8 +0,0 @@
-
-    #if(not file):
-  #   	loop = asyncio.get_event_loop()
-   #	tasks = asyncio.gather(
-  #  			download_wiki('wiki/Évariste_Galois'),
-  #   			download_wiki('wiki/Alan_Turing'),
-  #   			download_wiki('zzz'),
-		# )
